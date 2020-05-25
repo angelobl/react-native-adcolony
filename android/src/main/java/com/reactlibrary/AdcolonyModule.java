eb1713ff11ab1f8e72d294e5241820d545405d8e
Angelo Bertello <bertello.angelo@gmail.com>
2020-05-24 20:28:49 -0500

    Added events for AdColonyInterstitialListener callbacks

--- android/src/main/java/com/reactlibrary/AdcolonyModule.java
+++ android/src/main/java/com/reactlibrary/AdcolonyModule.java
@@ -4,6 +4,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
 import com.adcolony.sdk.AdColony;
 import com.adcolony.sdk.AdColonyAdOptions;
@@ -35,7 +38,14 @@ public class AdcolonyModule extends ReactContextBaseJavaModule {
         this.reactContext = reactContext;
     }
 
+    private void sendEvent(ReactApplicationContext context, String eventName, final WritableMap params) {
+      context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+  }
+
     private void onConfigure(final String appId,final String zoneId,final String userId) {
+      final WritableMap params1 = Arguments.createMap();
+        //final WritableMap params2 = Arguments.createMap();
+        //final WritableMap params3 = Arguments.createMap();
       String consent = "1";
       AdColonyAppOptions appOptions = new AdColonyAppOptions().setUserID(userId).setKeepScreenOn(true).setGDPRConsentString(consent).setGDPRRequired(true); 
       //.setKeepScreenOn(true);
@@ -45,11 +55,34 @@ public class AdcolonyModule extends ReactContextBaseJavaModule {
         @Override
         public void onRequestFilled(AdColonyInterstitial add) {
             // Ad passed back in request filled callback, ad can now be shown
+            params1.putBoolean("adRequestFilled", true);
+            sendEvent(reactContext, "Event", params1);
             ad = add;
 
         }
-        
-        
+        /*
+        @Override
+        public void onRequestNotFilled(AdColonyZone zone) {
+            // Ad request was not filled
+            params2.putBoolean("adRequestNotFilled", true);
+                    sendEvent(reactContext, "Event", params2);
+        }
+
+        @Override
+        public void onOpened(AdColonyInterstitial ad) {
+            // Ad opened, reset UI to reflect state change
+            showButton.setEnabled(false);
+            progress.setVisibility(View.VISIBLE);
+            Log.d(TAG, "onOpened");
+        }
+
+        @Override
+        public void onExpiring(AdColonyInterstitial ad) {
+            // Request a new ad if ad is expiring
+            params3.putBoolean("adExpired", true);
+                    sendEvent(reactContext, "Event", params3);
+        }
+    */    
 
       };
       //AdColony.setRewardListener(listener);
